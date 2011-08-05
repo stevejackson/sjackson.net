ff44e3d077c2a5aaff648a7294e61b9a0721a90c
Steve Jackson <steven.j.jackson@gmail.com>
2011-08-05 18:08:57 -0400

    Added jquery.kwicks plugin

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,20 @@
 // remap jQuery to $
 (function($){})(window.jQuery);
 
+alert('test2');
 
 /* trigger when page is ready */
 $(document).ready(function (){
 
-	// your functions go here
+	alert('moon landing');
+	$().ready(function() {
+		alert('test');
+		$('.navsub').html("test");
+		$('.navsub').kwicks({
+			max: 205,
+			spacing: 5
+		});
+	}
 
 });
 
