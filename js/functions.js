729331a2404807e0dcf68dc0567987e28395b12f
Steve Jackson <steven.j.jackson@gmail.com>
2011-08-05 21:56:37 -0400

    Nav improvements, misc. layout

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,11 @@
-// remap jQuery to $
-(function($){})(window.jQuery);
-
-alert('test2');
 
 /* trigger when page is ready */
 $(document).ready(function (){
 
-	alert('moon landing');
-	$().ready(function() {
-		alert('test');
-		$('.navsub').html("test");
-		$('.navsub').kwicks({
-			max: 205,
-			spacing: 5
-		});
-	}
+	$('.navsub').kwicks({
+		max: 150,
+		min: 80, 
+		spacing: 5
+	});
 
 });
-
-
-/* optional triggers
-
-$(window).load(function() {
-	
-});
-
-$(window).resize(function() {
-	
-});
-
-*/
