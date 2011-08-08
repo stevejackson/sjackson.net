fd34b1122ba6728aba12920b5c894125394c4a55
Steve Jackson <steven.j.jackson@gmail.com>
2011-08-08 08:45:10 -0400

    jquery changes

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,28 @@
 /* trigger when page is ready */
 $(document).ready(function (){
 
+	colorCycle();
+
+	function colorCycle() {
+		var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+
+		$('#box').animate({
+			width: "100%", 
+			backgroundColor: "blue",
+		}, 1000);
+	}
+
 	$('.navsub').kwicks({
 		max: 150,
 		min: 80, 
 		spacing: 5
+	});
+
+	$('#box').hover(function() {
+		$('#box').animate({
+			width: "50px",
+			opacity: 1.0,
+		}, 100);
 	});
 
 });
