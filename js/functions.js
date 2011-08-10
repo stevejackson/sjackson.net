1b8cb5079ae675d35ca8d7a10903d7bc45acb0e7
Steve Jackson <steven.j.jackson@gmail.com>
2011-08-09 21:20:02 -0400

    added social media icons

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,13 @@
 $(document).ready(function (){
 
+  // the hover over effect on navbar
   $('.navsub').kwicks({
     max: 150,
     min: 80, 
     spacing: 15
   });
 
+  // defin the hover in/out property on the sjj box
   $('#boxcontainer').hover(function() {
     $('#box').stop().animate({
       width: "50px",
@@ -19,18 +21,32 @@ $(document).ready(function (){
     }, 1000);
   });
 
+  // when the site loads, animate sjj box to the left
   $('#box').animate({
     width: "172px",
   }, 1000);
 
-  colorCycle();
+  // hover over effect for social icons
+  $('.iconcontainer').hover(function() {
+    $(this).animate({
+      opacity: 1.0,
+    }, 500);
+  },
+  function() {
+    $(this).animate({
+      opacity: 0.5,
+    }, 500);
+  });
 
-  function colorCycle() {
-    var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  socialIcons();
 
-    $('#box').animate({
-    }, 2500);
-
+  function socialIcons() {
+    $('.iconcontainer').each(function(index) {
+      $(this).animate({
+        opacity: 0.5,
+        top: 0,
+      }, 2000);
+    });
   }
 
 });
